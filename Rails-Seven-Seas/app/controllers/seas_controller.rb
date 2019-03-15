d435e4ea16b6fbbae8f2491773ad7bc38cf80513
James Dorr <jamesdorr3@gmail.com>
2019-03-15 19:06:47 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,47 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  def welcome
+  end
+
+  def index
+    @seas = Sea.all
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def create
+    @sea = Sea.create(params)
+    redirect_to
+    # if @sea.save
+    #   redirect_to @sea
+    # else
+    #   # This line overrides the default rendering behavior, which
+    #   # would have been to render the "create" view.
+    #   render "new"
+    # end
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+  end
+
+  def destroy
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
